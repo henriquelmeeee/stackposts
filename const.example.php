be9bb84848567b243ccc073419bab92ced03b761
Hacker Gaucho <84669018+hackergaucho@users.noreply.github.com>
2022-07-07 20:19:26 -0300

    exemplo de configuração

---
+++ const.example.php
@@ -0,0 +1,11 @@
+<?php
+
+define('DEFAULT_LANGUAGE', 'en');
+define('MAINTENANCE_MODE', 0);
+define('MYSQL_HOST', 'localhost');
+define('MYSQL_DATABASE', 'twitcode');
+define('MYSQL_USERNAME', 'root');
+define('MYSQL_PASSWORD', '');
+define('MYSQL_PORT', '3306');
+define('SITE_LANGUAGES', 'en,pt');
+define('SITE_URL', 'http://localhost');
