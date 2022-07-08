75629e63750d914f6e9a3efdaf142ea68c7b2411
Henrique <86432400+henriquelmeeee@users.noreply.github.com>
2022-07-07 22:29:43 -0300

    Update home.php

--- view/home.php
+++ view/home.php
@@ -14,14 +14,17 @@ view('inc/head', $data);
 <?php
 view('form/message');
 ?><br>
-<a  class="btn btn-primary" href="?random">Aleatório <i class="fas fa-external-link-alt"></i></a> <button class="transparentButton" onclick="document.location='?random'"><i class='fas fa-redo-alt' style='font-size: 25px;'></i></button>
+<a  class="btn btn-primary" href="?random">Aleatório <i class="fas fa-external-link-alt"></i></a> <button class="transparentButton" on>
 
 <?php
 if ($message) {
-    print '<p id="content" =>'.e($message['message'],false).'</p>';
+    print '<p style="margin-right: 530px;" id="content" =>'.e($message['message'],false).'</p>';
     // $href='/messages/'.$message['id'];
     // print '<a href="'.$href.'">';
     // print '<small>'.date('r', $message['created_at']).'</small></a>';
 }
 ?>
     </div>
+
+
+
